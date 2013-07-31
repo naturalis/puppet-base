b4f5a579cf4b65e31d3452fb7c295854b684eeb6
Paul Gomersbach <p.gomersbach@gmail.com>
2013-07-31 14:46:07 +0200

    add centos packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,15 +1,15 @@
 class base::packages {
   case $operatingsystem {
     centos, redhat: {
-      singleton_packages( 'wget' )
+      singleton_packages( 'wget', 'vim-enhanced' )
     }
     debian, ubuntu: {
-      singleton_packages( 'curl' )
+      singleton_packages( 'curl', 'vim', 'htop', íftop' )
     }
     default: {
       fail("Unrecognized operating system")
     }
   }
   # Common packages
-  singleton_packages( 'sudo', 'screen', 'htop', 'iftop', 'vim', 'zsh', 'mc')
+  singleton_packages( 'sudo', 'screen', 'zsh', 'mc')
 }
