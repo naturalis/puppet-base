3409c38f3f871459f9769ebf358584cfb0c42cae
Paul Gomersbach <p.gomersbach@gmail.com>
2013-07-31 16:27:21 +0200

    remove singleton from centos

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,7 +2,7 @@ class base::packages {
   case $operatingsystem {
     centos, redhat: {
       include repoforge
-      singleton_packages( 'sudo', 'screen', 'zsh', 'mc', 'wget', 'vim-enhanced', 'htop' )
+#      singleton_packages( 'sudo', 'screen', 'zsh', 'mc', 'wget', 'vim-enhanced', 'htop' )
     }
     debian, ubuntu: {
       singleton_packages( 'sudo', 'screen', 'zsh', 'mc', 'curl', 'vim', 'htop', 'iftop' )
