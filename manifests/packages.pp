2690d91078adcc459413bf95b2310755a06e0c0f
Paul Gomersbach <p.gomersbach@gmail.com>
2013-07-31 16:16:52 +0200

    remove standard packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,15 +2,13 @@ class base::packages {
   case $operatingsystem {
     centos, redhat: {
       include repoforge
-      singleton_packages( 'wget', 'vim-enhanced', 'htop' )
+      singleton_packages( 'sudo', 'screen', 'zsh', 'mc', 'wget', 'vim-enhanced', 'htop' )
     }
     debian, ubuntu: {
-      singleton_packages( 'curl', 'vim', 'htop', 'iftop' )
+      singleton_packages( 'sudo', 'screen', 'zsh', 'mc', 'curl', 'vim', 'htop', 'iftop' )
     }
     default: {
       fail("Unrecognized operating system")
     }
   }
-  # Common packages
-  singleton_packages( 'sudo', 'screen', 'zsh', 'mc')
 }
