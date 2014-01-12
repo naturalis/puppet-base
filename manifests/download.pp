a33b8754f1d064663d4376a47ca13db14cabe73d
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2014-01-12 13:19:29 +0100

    Update download.pp

--- manifests/download.pp
+++ manifests/download.pp
@@ -1,10 +1,10 @@
 # Download a file from a given url
 #
-define base::download ($uri, $timeout = 300) {
+define base::download ($uri, $contains, $timeout = 300) {
   exec { "download ${uri}":
     command => "/usr/bin/wget -N -q '${uri}' -O ${name}",
     timeout => $timeout,
     require => Class[ 'base::packages' ],
-    unless  => "/usr/bin/test -f ${name}"
+    unless  => "/bin/grep ${contains} ${name}"
   }
 }
