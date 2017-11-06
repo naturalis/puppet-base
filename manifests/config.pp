61196d0cc1498ef620c75abd280309e3d6992739
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2017-11-06 15:37:58 +0000

    fix for puppet4

--- manifests/config.pp
+++ manifests/config.pp
@@ -29,19 +29,19 @@ class base::config {
       ],
   }
 
-  download { "/etc/screenrc":
+  base::download { "/etc/screenrc":
     uri	     => 'http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic',
     timeout  => 900,
     contains => 'grml'
   }
 
-  download { "/etc/vimrc":
+  base::download { "/etc/vimrc":
     uri	     => 'http://git.grml.org/f/grml-etc-core/etc/vim/vimrc',
     timeout  => 900,
     contains => 'grml'
   }
 
-  download { "/etc/zshrc":
+  base::download { "/etc/zshrc":
     uri      => 'http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc',
     timeout  => 900,
     contains => 'grml'
