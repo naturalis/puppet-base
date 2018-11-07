b5e842b0e8894c38f01b13bef42a3598a365f00a
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-11-07 11:54:39 +0000

    added link for puppet and facter

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,8 +4,8 @@ class base::config {
 
   group { 'wheel': ensure => present, }
 
-  augeas { "sudowheel":
-    context => "/files/etc/sudoers",
+  augeas { 'sudowheel':
+    context => '/files/etc/sudoers',
     changes => [
       "set spec[user = '%wheel']/user %wheel",
       "set spec[user = '%wheel']/host_group/host ALL",
@@ -15,77 +15,87 @@ class base::config {
       ],
   }
 
-  augeas { "sshdns":
-    context => "/files/etc/ssh/sshd_config",
+  augeas { 'sshdns':
+    context => '/files/etc/ssh/sshd_config',
     changes => [
         "set UseDNS 'no'",
       ],
   }
 
-  augeas { "sshGSSAPIauth":
-    context => "/files/etc/ssh/sshd_config",
+  augeas { 'sshGSSAPIauth':
+    context => '/files/etc/ssh/sshd_config',
     changes => [
         "set GSSAPIAuthentication 'no'",
       ],
   }
 
-  base::download { "/etc/screenrc":
+  base::download { '/etc/screenrc':
     uri	     => 'http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic',
     timeout  => 900,
     contains => 'grml'
   }
 
-  base::download { "/etc/vimrc":
+  base::download { '/etc/vimrc':
     uri	     => 'http://git.grml.org/f/grml-etc-core/etc/vim/vimrc',
     timeout  => 900,
     contains => 'grml'
   }
 
-  base::download { "/etc/zshrc":
+  base::download { '/etc/zshrc':
     uri      => 'http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc',
     timeout  => 900,
     contains => 'grml'
   }
 
-  file { "/usr/share/mc/syntax/puppet.syntax":
-    source  => "puppet:///modules/base/mc_puppet.syntax",
+  file { '/usr/share/mc/syntax/puppet.syntax':
+    source  => 'puppet:///modules/base/mc_puppet.syntax',
     require => Package['mc']
   }
 
-  file { "/usr/share/mc/syntax/Syntax":
-    source  => "puppet:///modules/base/mc_Syntax",
+  file { '/usr/share/mc/syntax/Syntax':
+    source  => 'puppet:///modules/base/mc_Syntax',
     require => Package['mc']
   }
 
-  file { "/root/.config":
+  file { '/root/.config':
     ensure  => directory,
     mode    => '0600'
   }
 
-  file { "/root/.config/mc":
+  file { '/root/.config/mc':
     ensure  => directory,
     mode    => '0600',
     require => File['/root/.config']
   }
 
-  file { "/root/.config/mc/ini":
-    source  => "puppet:///modules/base/mc_ini",
-    replace => "no",
-    ensure  => "present",
+  file { '/root/.config/mc/ini':
+    source  => 'puppet:///modules/base/mc_ini',
+    replace => 'no',
+    ensure  => 'present',
     mode    => '0600',
     require => File['/root/.config/mc']
   }
 
-  if $osfamily == "debian" {
-    exec { "localegen":
-      command => "/usr/sbin/locale-gen nl_NL.UTF-8",
-      path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
-      unless  => "/usr/bin/locale -a | /bin/grep nl_NL.utf8"
+  file { '/usr/bin/puppet':
+    ensure  => link,
+    target  => '/opt/puppetlabs/bin/puppet'
+  }
+
+  file { '/usr/bin/facter':
+    ensure  => link,
+    target  => '/opt/puppetlabs/bin/facter'
+  }
+
+  if $osfamily == 'debian' {
+    exec { 'localegen':
+      command => '/usr/sbin/locale-gen nl_NL.UTF-8',
+      path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      unless  => '/usr/bin/locale -a | /bin/grep nl_NL.utf8'
     }
   }
 
-  exec {"fix_fqdn":
+  exec {'fix_fqdn':
     command         => "/bin/echo '127.0.0.1   ${hostname}.${domain} ${hostname}' >> /etc/hosts",
-    unless          => "/bin/hostname -f"
+    unless          => '/bin/hostname -f'
   }
 }
